1b2512ba2718903094d535cec802ab097d530498
Fernando Tello <feranto31@hotmail.com>
2023-10-07 18:30:19 -0500

    Add fonts, Login draft

--- Caii-2023/ViewController.swift
+++ Caii-2023/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputDNI: UITextField!
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func onLoginPressed(_ sender: UIButton) {
+        
+        if(inputDNI.text == "1"){
+            print("holas")
+            // go to next storyboard
+        }else{
+            let alert = UIAlertController(title: "Hey", message: "This is  one Alert", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Working!!", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+    }
 }
 
